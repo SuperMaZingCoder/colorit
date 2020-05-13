daed039ad63655daeb6e90b916279eb9b9cef503
BeastCoder <reed.manning06@gmail.com>
2020-05-13 09:44:49 -0400

    Made functions easier to read

--- ColorIt.py
+++ ColorIt.py
@@ -20,8 +20,8 @@ def initColorIt():
 
 
 def color(text, rgb):
-    return "\033[38;2;" + str(rgb[0]) + ";" + str(rgb[1]) + ";" + str(rgb[2]) + "m" + text + "\033[0m"
+    return "\033[38;2;{};{};{}m{}\033[0m".format(str(rgb[0]), str(rgb[1]), str(rgb[2]), text)
 
 
 def background(text, rgb):
-    return "\033[48;2;" + str(rgb[0]) + ";" + str(rgb[1]) + ";" + str(rgb[2]) + "m" + text + "\033[0m"
+    return "\033[48;2;{};{};{}m{}\033[0m".format(str(rgb[0]), str(rgb[1]), str(rgb[2]), text)
